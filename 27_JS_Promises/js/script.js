f1a308db3c285ef994741425efcf619236526188
codewthjava <abhisunnii@gmail.com>
2023-08-16 10:10:59 +0530

    promises

---
+++ 27_JS_Promises/js/script.js
@@ -0,0 +1,130 @@
+console.log("Promises");
+
+
+/***
+ *
+ *  HTTP Methods
+ *
+ *   Request
+ *   Response
+ *   RequestBody
+ *   Path Param
+ *   Query Param
+ *
+ *
+ *
+ */
+
+/**
+ *      http      https
+ *      Hyper text transfer protocol           Hyper text transfer protocol  security
+ *
+ *
+ *        https://www.youtube.com/search?searchText="rrr videos";
+ *        https://www.youtube.com/search/string or number
+ *
+ *
+ *
+ */
+
+
+
+//Promise is asyncronus programming
+
+/**
+ *          asyncronus     /  syncronus programming
+ * 
+ * 
+ */
+let isExecuted = true;
+let loadingText = "";
+
+let promise = new Promise((resolve, reject) => {
+    if (isExecuted == true) {
+        loadingText = "Loading";
+        console.log(loadingText);
+        resolve("Resolve sucessfully")
+
+        setTimeout(() => {
+            // printNumbers();
+            loadingText = "";
+        }, 10000)
+
+    } else {
+        reject("Rejected Successfully");
+    }
+}).then((success) => {
+    console.log(success)
+}).catch((error) => {
+    console.log(error)
+})
+
+
+
+function printNumbers() {
+    for (let i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+}
+
+
+
+let GET_ALL_USERS = "https://jsonplaceholder.typicode.com/users";
+/* <tr>
+    <td>1</td>
+</tr> */
+//  <tr>
+//     <td>sai</td>
+//  </tr>
+
+//}
+let tbody = document.getElementById('tbody');
+
+
+function getAllUsers() {
+    new Promise(() => {
+        fetch(GET_ALL_USERS).then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data);
+            let users = [...data];
+            users.sort((a, b) => a.username.localeCompare(b.username));
+            users.sort((a, b) => a.email.localeCompare(b.email));
+            users.sort((a, b) => a.name.localeCompare(b.name));
+            users.forEach((user) => {
+                let { id, name, username, email } = user;
+                let tr = document.createElement("tr");
+
+
+                let idData = document.createElement("td");
+                idData.textContent = id;
+                tr.append(idData);
+
+
+                let nameData = document.createElement("td");
+                nameData.textContent = name;
+                tr.append(nameData);
+
+
+                let usernameData = document.createElement("td");
+                usernameData.textContent = username;
+                tr.append(usernameData);
+
+                let emailData = document.createElement("td");
+                emailData.textContent = email;
+                tr.append(emailData);
+
+                tbody.append(tr);
+            })
+
+            console.log(users);
+
+
+        }).catch((error) => {
+            console.log(error);
+        })
+    })
+}
+
+getAllUsers();
+//()=>{}
